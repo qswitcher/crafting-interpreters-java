86f626c5235f8170bb8ae06b7417669f3be942c3
jrussom <jeffreyrussom@gmail.com>
2023-08-02 14:12:33 -0500

    Chapter 13

--- lox/src/com/craftinginterpreters/lox/Resolver.java
+++ lox/src/com/craftinginterpreters/lox/Resolver.java
@@ -29,7 +29,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 	}
 
 	private enum ClassType {
-		NONE, CLASS
+		NONE, CLASS, SUBCLASS
 	}
 
 	private ClassType currentClass = ClassType.NONE;
@@ -127,7 +127,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 			if (currentFunction == FunctionType.INITIALIZER) {
 				Lox.error(stmt.keyword, "Can't return a value from an initializer.");
 			}
-			
+
 			resolve(stmt.value);
 		}
 
@@ -253,6 +253,20 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 		declare(stmt.name);
 		define(stmt.name);
 
+		if (stmt.superclass != null && stmt.name.lexeme.equals(stmt.superclass.name.lexeme)) {
+			Lox.error(stmt.superclass.name, "A class can't inherit from itself.");
+		}
+
+		if (stmt.superclass != null) {
+			currentClass = ClassType.SUBCLASS;
+			resolve(stmt.superclass);
+		}
+
+		if (stmt.superclass != null) {
+			beginScope();
+			scopes.peek().put("super", true);
+		}
+
 		beginScope();
 		scopes.peek().put("this", true);
 
@@ -266,6 +280,9 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 		}
 
 		endScope();
+
+		if (stmt.superclass != null)
+			endScope();
 
 		currentClass = enclosingClass;
 		return null;
@@ -289,6 +306,18 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 		if (currentClass == ClassType.NONE) {
 			Lox.error(expr.keyword, "Can't use 'this' outside of a class.");
 			return null;
+		}
+
+		resolveLocal(expr, expr.keyword);
+		return null;
+	}
+
+	@Override
+	public Void visitSuperExpr(Expr.Super expr) {
+		if (currentClass == ClassType.NONE) {
+			Lox.error(expr.keyword, "Can't use 'super' outside of a class.");
+		} else if (currentClass != ClassType.SUBCLASS) {
+			Lox.error(expr.keyword, "Can't use 'super' in a class with no superclass.");
 		}
 
 		resolveLocal(expr, expr.keyword);
